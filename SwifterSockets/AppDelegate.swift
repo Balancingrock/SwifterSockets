19a629dc8628d8d349d208f6df99c5288ba26c32
Rien <lugt@xs4all.nl>
2016-03-02 10:59:20 +0100

    Initial Commit

---
+++ SwifterSockets/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SwifterSockets
+//
+//  Created by Marinus van der Lugt on 02/03/16.
+//  Copyright © 2016 Marinus van der Lugt. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
